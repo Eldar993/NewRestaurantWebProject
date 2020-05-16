f14d4b89ac8a12674ca061c6f01cae7db65cf567
Eldar <eldar7989@gmail.com>
2020-05-16 19:20:48 +0200

    Add login form

--- src/main/java/com/example/restaurant/controller/UserController.java
+++ src/main/java/com/example/restaurant/controller/UserController.java
@@ -7,10 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -21,14 +18,14 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/create", method = RequestMethod.GET)
+   /* @RequestMapping(value = "/create", method = RequestMethod.GET)
     public ModelAndView createUser(ModelAndView mav) {
         User createdUser = userService.create();
         mav.setViewName("create");
         mav.addObject("jamesBond", createdUser);
 
         return mav;
-    }
+    }*/
     @RequestMapping(value = "/print_all", method = RequestMethod.GET)
     @ResponseBody
     public ModelAndView printAll(ModelAndView mav) {
@@ -52,11 +49,14 @@ public class UserController {
 
     @RequestMapping(value = "/login",method = RequestMethod.POST)
     @ResponseBody
-    public ModelAndView login(@ModelAttribute("user") User user,
+    public ModelAndView login(@ModelAttribute("user") User user,@RequestParam("name") String name,@RequestParam("password") String password,
                         BindingResult result, ModelAndView mav){
 
-        System.out.println("login: " + user.getName());
-        System.out.println("password: " + user.getPassword());
+        user.setName(name);
+        user.setPassword(password);
+        mav.addObject("name",name);
+        mav.addObject("password",password);
+        userService.saveNewUser(user);
 
         mav.setViewName("/login");
 
--- src/main/java/com/example/restaurant/service/UserService.java
+++ src/main/java/com/example/restaurant/service/UserService.java
@@ -2,8 +2,11 @@ package com.example.restaurant.service;
 
 import com.example.restaurant.entity.User;
 import com.example.restaurant.repository.UserRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Service;
 
+import javax.sql.DataSource;
 import javax.transaction.Transactional;
 import java.util.List;
 
@@ -17,14 +20,20 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public User create() {
-        User user = new User();
-        user.setName(generateRandomString(NAME_LENGTH));
-        user.setPassword(generateRandomString(NAME_LENGTH*3));
+    /*public User create(User user) {
+        *//*user.setName(generateRandomString(NAME_LENGTH));
+        user.setPassword(generateRandomString(NAME_LENGTH*3));*//*
 
+    }*/
+    public User saveNewUser(User user) {
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(datasource);
+
+        String sql = "";
+        jdbcTemplate.update(sql,user.getName(), user.getPassword());
         return userRepository.saveAndFlush(user);
-    }
 
+    }
     private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
     public String generateRandomString(int count) {
