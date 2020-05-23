c22a556e57dde795d6acd7abf2a8d9740554a066
Eldar <eldar7989@gmail.com>
2020-05-23 10:09:13 +0200

    add IngredientDto and modelMapper.add modelMapper dependency in pom.xml

---
+++ src/main/java/com/example/restaurant/dto/IngredientDto.java
@@ -0,0 +1,40 @@
+package com.example.restaurant.dto;
+
+import com.example.restaurant.entity.Ingredient;
+
+public class IngredientDto {
+    private Long id;
+    private String title;
+    private int calories;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    
+    public Ingredient _toDto(){
+        Ingredient ingredient = new Ingredient();
+        //set entity values here from StudentDTO
+        return ingredient ;
+    }
+}
