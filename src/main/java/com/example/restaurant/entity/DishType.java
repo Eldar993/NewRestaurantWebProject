4ce0b0f75ef2aed7ab8d598d21a7cb508105403d
JIeIIIa <no-reply@mail.com>
2020-05-24 20:52:56 +0300

    Add html tags into header template

--- src/main/java/com/example/restaurant/entity/DishType.java
+++ src/main/java/com/example/restaurant/entity/DishType.java
@@ -18,22 +18,6 @@ public class DishType {
     @Column(unique=true)
     private String title;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
