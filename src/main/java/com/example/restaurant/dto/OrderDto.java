d4c2827cb307e06d710897223b3fb4633012db1f
Eldar <eldar7989@gmail.com>
2020-06-01 13:08:19 +0200

    Change OrderDto user field

--- src/main/java/com/example/restaurant/dto/OrderDto.java
+++ src/main/java/com/example/restaurant/dto/OrderDto.java
@@ -1,11 +1,13 @@
 package com.example.restaurant.dto;
 
+import com.example.restaurant.entity.User;
+
 import java.time.LocalDateTime;
 
 public class OrderDto {
     private Long id;
     private LocalDateTime createdAt;
-    private Long user_id;
+    private User user;
 
     public Long getId() {
         return id;
@@ -23,11 +25,11 @@ public class OrderDto {
         this.createdAt = createdAt;
     }
 
-    public Long getUser_id() {
-        return user_id;
+    public User getUser() {
+        return user;
     }
 
-    public void setUser_id(Long user_id) {
-        this.user_id = user_id;
+    public void setUser(User user) {
+        this.user = user;
     }
 }
