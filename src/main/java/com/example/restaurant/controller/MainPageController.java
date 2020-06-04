bc7a5c690b79006240ce4e0dde5e519d1aecfcb2
JIeIIIa <no-reply@mail.com>
2020-06-04 22:52:38 +0300

    Add "/" mapping for main page

--- src/main/java/com/example/restaurant/controller/MainPageController.java
+++ src/main/java/com/example/restaurant/controller/MainPageController.java
@@ -3,24 +3,21 @@ package com.example.restaurant.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainPageController {
-    @RequestMapping(value = "/main", method =  RequestMethod.GET)
-        public ModelAndView mainPage(ModelAndView mav){
-            mav.setViewName("/main");
-            return mav;
-        }
-
-    @RequestMapping(value = "/basket", method =  RequestMethod.GET)
-    public ModelAndView basketPage(ModelAndView mav){
-        mav.setViewName("/basket");
+    @RequestMapping(value = {"/", "/main"}, method = RequestMethod.GET)
+    public ModelAndView mainPage(ModelAndView mav) {
+        mav.setViewName("/main");
         return mav;
     }
 
-
+    @RequestMapping(value = "/basket", method = RequestMethod.GET)
+    public ModelAndView basketPage(ModelAndView mav) {
+        mav.setViewName("/basket");
+        return mav;
+    }
 
 
 }
